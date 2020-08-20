17ce166cf7619da458a199e76d6fcc2ff8981b05
Fabiano Belem <famube@gmail.com>
2020-08-20 11:09:24 -0300

    Script para rodar em arquivos html

---
+++ scripts/runall_html.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+dir=$1
+outdir=/datalake/ufmg/m02
+
+#Reproduzir arvore de diretorios
+
+find "$dir" -type d | while read d
+do
+    outsubdir=`echo "$d" | cut -d "/" -f 1,2,3 --complement`
+    echo "$outdir/$outsubdir"
+    mkdir -p "$outdir/$outsubdir"
+
+    find "$d" -maxdepth 1 -type f -name "*.html" | while read f
+    do
+       echo "$f"
+       filename=`basename "$f" ".html"`
+       out="$outdir/$outsubdir/$filename"
+       
+       python3 -m html2text --ignore-links --ignore-images "$f" utf-8 > "$out.txt"
+       python3 -m preprocessing.text_cleaner "$out.txt" "$out.clean"
+
+       ./run_pipeline.sh "$out.clean" "$out.entidades"
+    done
+
+done
+
