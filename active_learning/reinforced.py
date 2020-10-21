4b9c769d87d266c2f6fa5554b3626f606c911a63
Fabiano Muniz Belem <famube@gmail.com>
2020-10-21 17:37:24 -0300

    Active Learning + Reinforcement Learning

---
+++ active_learning/reinforced.py
@@ -0,0 +1,260 @@
+import sys, os
+
+from inout import load_conll, load_conll_probs
+from scipy.stats import entropy
+from ml.categorical_feat_reader import load_feats, to_text
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_distances, cosine_similarity
+from numpy import array, mean, zeros, dot
+from numpy.random import rand, randint
+from inout import load_set
+from operator import itemgetter
+from math import log
+
+def pre_select(mat, labels, last_unlabeled_index, n=1):
+    res = []
+    indexes = []
+    sent_indexes = []
+    entropies = []
+    prob_ent = []
+
+    j = 0
+    for sent_ind,labs in enumerate(labels):
+        token_entropy = []
+        for lab in labs:
+            start,end,lab,probs = lab
+            h = entropy(list(probs.values()))
+            p = 1 - probs["O"]
+            token_entropy.append( (h, p, j) )
+            j += 1
+            #if j == last_unlabeled_index:
+            #    n = 10
+        for (entr, p, mat_ind) in sorted(token_entropy, reverse=True)[:n]:
+            res.append(mat[mat_ind])
+            sent_indexes.append(sent_ind)
+            indexes.append(mat_ind)
+            entropies.append(entr)
+            prob_ent.append(p)
+    return res, sent_indexes, indexes, entropies, prob_ent
+
+
+def extract_f1(filename):
+    infile = open(filename)
+    f1 = -1
+    for line in infile:
+        if "Over" in line:
+            spl = line.strip().split()
+            f1 = float(spl[6])/100
+    infile.close()
+    return f1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 10:
+        print ("usage: %s <unlabeled set (U)> <label probabilities of U> <features of U> \n \
+                          <labeled set (L)> <label probabilities of L> <features of L> \n \
+                          <validation set (V)> <out path> <n (tokens to consider per sentence> \n \
+                          <similarity thresh> [fraction of U to select]" % sys.argv[0])
+        sys.exit(-1)
+
+
+    unlab_filename = sys.argv[1]
+    unlabprob_filename = sys.argv[2]
+    unlabfeats_filename = sys.argv[3]
+
+    lab_filename = sys.argv[4]
+    labprob_filename = sys.argv[5]
+    labfeats_filename = sys.argv[6]
+    valid_filename = sys.argv[7]
+    outpath = sys.argv[8]
+
+    #"Unlabeled" dataset
+    sents,labels = load_conll_probs(unlabprob_filename)
+    mat = load_feats(unlabfeats_filename)
+    sents,true_labels = load_conll(unlab_filename, col=2)
+
+    #"Labeled" dataset
+    Lsents,Llabels = load_conll_probs(labprob_filename)
+    Lmat = load_feats(labfeats_filename)
+    Lsents,Ltrue_labels = load_conll(lab_filename, col=2)
+
+    last_unlab_index = len(mat) - 1
+
+    n = int(sys.argv[9])
+    entr_thresh = 0.6
+    sim_thresh = 0.8
+    frac = 0.1
+    lr = 1
+
+    if len(sys.argv) == 12:
+        frac = float(sys.argv[11])
+    sim_thresh = float(sys.argv[10])
+
+    #out = open(outpath, "w", encoding="utf-8")
+
+    dist_thresh = 1 - sim_thresh
+
+    print("unlabeled examples:", unlab_filename, file=sys.stderr)
+    print("label probabilities of the unlabeled examples:", unlabprob_filename, file=sys.stderr)
+    print("features of the unlabeled examples", unlabfeats_filename, file=sys.stderr)
+
+    print("labeled examples:", lab_filename, file=sys.stderr)
+    print("label probabilities of the labeled examples:", labprob_filename, file=sys.stderr)
+    print("features of the labeled examples", labfeats_filename, file=sys.stderr)
+
+    print("outpath:", outpath, file=sys.stderr)
+    print("n:", n, file=sys.stderr)
+    print("sim_thresh:", sim_thresh, file=sys.stderr)
+
+    print("|U| =", len(mat))
+    print("|L| =", len(Lmat))
+
+    print("#U sentences =", len(labels))
+    print("#L sentences =", len(Llabels))
+
+    print("U ntokens= ", sum([len(x) for x in labels]))
+    print("L ntokens= ", sum([len(x) for x in Llabels]))
+
+    mat += Lmat
+    labels += Llabels
+
+    print("|L| + |U| =", len(mat))
+    print("#U+L sentences =", len(labels))
+
+    print("ntokens= ", sum([len(x) for x in labels]))
+
+    mat_filtered, indexes_sent, indexes, entropies, prob_ent = pre_select(mat, labels, last_unlab_index, n=n)
+    print("len(mat)=", len(mat))
+
+    mat_text = to_text(mat_filtered)
+
+    print("len(mat_filtered)=", len(mat_filtered))
+    #vectorizer = TfidfVectorizer(lowercase=False)
+    vectorizer = CountVectorizer(lowercase=False)
+    mat_filtered = vectorizer.fit_transform(mat_text)
+
+
+    nselect = int(frac * len(sents))
+
+
+
+    print("Computing similarities...")
+    sim = cosine_distances(mat_filtered)
+
+    print("Clustering...")
+    #clusterer = AgglomerativeClustering(n_clusters=nselect, affinity="precomputed", linkage="average")
+    clusterer = AgglomerativeClustering(n_clusters=None, affinity="precomputed", linkage="average", distance_threshold=dist_thresh)
+    clusters = clusterer.fit_predict(sim)
+
+    groups = {}
+
+    num_labeled_in_cluster = {}
+    cluster_score = {}
+    for i, c in enumerate(clusters):
+        if c not in groups:
+            num_labeled_in_cluster[c] = 0
+            cluster_score[c] = 0
+            groups[c] = []
+        if indexes[i] > last_unlab_index:
+            num_labeled_in_cluster[c] += 1
+        groups[c].append(i)
+
+    larger_cluster_size = max([len(g) for g in groups.values()])
+    log_larger_cluster_size = log(larger_cluster_size)
+    NFEATURES = 3
+    f1_ant = 0.2369
+
+    #Inicializar w com pesos aleatorios
+    w = rand(NFEATURES)
+    S = 20 #tamanho de cada mini-amostra
+    T = 50
+    A = 5 #numero de mini-amostras
+    epsilon = 0.2
+    SA = S*A
+    x_cluster = {}
+    print("w:", w)
+
+    for t in range(T):
+        X = []
+        selected = []
+        for i, c in enumerate(clusters):
+            x = [entropies[i], prob_ent[i], log(len(groups[c]))/log_larger_cluster_size]
+            sc = dot(w, array(x))
+            if sc > cluster_score[c]:
+                cluster_score[c] = sc
+                x_cluster[c] = (i, x)
+
+        for c, sc in sorted(cluster_score.items(), key=itemgetter(1), reverse=True):
+            if len(selected) > SA:
+                break
+            if rand() < epsilon:
+                c = randint(0, len(groups))
+            if num_labeled_in_cluster[c] == 0:
+                i,x = x_cluster[c]
+                X.append(x)
+                selected.append(indexes_sent[i])
+                print("sent:", sents[indexes_sent[i]])
+                print("repr:", mat[indexes[i]])
+                print("x:", x)
+
+        gradient = zeros(NFEATURES)
+        avg_f1 = 0
+
+        #Para cada mini-amostra
+        for a in range(A):
+            subsample = X[a*S:(a+1)*S]
+            x_mean = mean(array(subsample), axis=0)
+
+            #Print selected sentences
+            out = open(outpath, "w", encoding="utf-8")
+            for i in selected[a*S:(a+1)*S]:
+                sent_labels = true_labels[i]
+                sent = sents[i]
+                for j, lab in enumerate(sent_labels):
+                    start,end,true_lab = lab
+                    tok = sent[start:end]
+                    print("%s\t%s" % (tok, true_lab), file=out)
+                print(file=out)
+            out.close()
+
+            cmd = "cat " +  lab_filename + "_sample5k " + outpath + " > " +  outpath + ".conll"
+            #cmd = "cat " + outpath + " > " +  outpath + ".conll"
+            #os.system("cat " + outpath + ".conll")
+
+            print(cmd)
+            os.system(cmd)
+
+            cmd = "train/make_prop.sh " + outpath + ".conll " + outpath + ".ser.gz > " + outpath + "_prop.txt"
+            print(cmd)
+            os.system(cmd)
+
+            cmd = "./train_corenlp.sh " + outpath + "_prop.txt"
+            print(cmd)
+            os.system(cmd)
+
+            cmd = "./test_corenlp.sh " + valid_filename + " " + outpath + ".ser.gz " + outpath + ".out"
+            print(cmd)
+            os.system(cmd)
+
+            cmd = "scripts/conlleval < " + outpath + ".out -d \"\\t\" -l > " + outpath + ".metrics"
+            print(cmd)
+            os.system(cmd)
+
+            f1 = extract_f1(outpath + ".metrics")
+            avg_f1 += f1
+            delta_f1 = f1 - f1_ant
+            gradient += delta_f1 * x_mean
+            print("x_mean:", x_mean)
+            print("DeltaF1:", delta_f1)
+            print("f1:", f1)
+
+        #Atualizar pesos
+        w += lr * (1/A) * gradient
+        avg_f1 /= A
+        f1_ant = avg_f1
+        print("avgF1:", avg_f1)
+        print("w:", w)
+        print("gradient:", gradient)
+
+
--- active_learning/select_cluster_based.py
+++ active_learning/select_cluster_based.py
@@ -10,7 +10,7 @@ from sklearn.metrics.pairwise import cosine_distances, cosine_similarity
 from numpy import array, mean, zeros
 from inout import load_set
 
-def pre_select(mat, labels, n=1):
+def pre_select(mat, labels, last_unlabeled_index, n=1):
     res = []
     indexes = []
     sent_indexes = []
@@ -21,9 +21,12 @@ def pre_select(mat, labels, n=1):
         for lab in labs:
             start,end,lab,probs = lab
             h = entropy(list(probs.values()))
-            token_entropy.append( (h, j) )
+            p = 1 - probs["O"]
+            token_entropy.append( (p, h, j) )
             j += 1
-        for (entr, mat_ind) in sorted(token_entropy, reverse=True)[:n]:
+            #if j == last_unlabeled_index:
+            #    n = 10
+        for (p, entr, mat_ind) in sorted(token_entropy, reverse=True)[:n]:
             res.append(mat[mat_ind])
             sent_indexes.append(sent_ind)
             indexes.append(mat_ind)
@@ -41,36 +44,76 @@ def filter_stopwords(mat, stopwords):
     return res, indexes
 
 
-#"Unlabeled" dataset
-sents,labels = load_conll_probs(sys.argv[2])
-mat = load_feats(sys.argv[3])
-sents,true_labels = load_conll(sys.argv[1], col=2)
 
-n = int(sys.argv[5])
+unlab_filename = sys.argv[1]
+unlabprob_filename = sys.argv[2]
+unlabfeats_filename = sys.argv[3]
+
+lab_filename = sys.argv[4]
+labprob_filename = sys.argv[5]
+labfeats_filename = sys.argv[6]
+outfilename = sys.argv[7]
+
+#"Unlabeled" dataset
+sents,labels = load_conll_probs(unlabprob_filename)
+mat = load_feats(unlabfeats_filename)
+sents,true_labels = load_conll(unlab_filename, col=2)
+
+#"Labeled" dataset
+Lsents,Llabels = load_conll_probs(labprob_filename)
+Lmat = load_feats(labfeats_filename)
+Lsents,Ltrue_labels = load_conll(lab_filename, col=2)
+
+last_unlab_index = len(mat) - 1
+
+n = int(sys.argv[8])
 entr_thresh = 0.6
 sim_thresh = 0.8
 frac = 0.1
-if len(sys.argv) == 9:
-    frac = float(sys.argv[8])
-if len(sys.argv) > 7:
-    entr_thresh = float(sys.argv[6])
-    sim_thresh = float(sys.argv[7])
+if len(sys.argv) == 12:
+    frac = float(sys.argv[11])
+if len(sys.argv) > 10:
+    entr_thresh = float(sys.argv[9])
+    sim_thresh = float(sys.argv[10])
 
-out = open(sys.argv[4], "w", encoding="utf-8")
+out = open(outfilename, "w", encoding="utf-8")
 
 dist_thresh = 1 - sim_thresh
 
-print("unlabeled examples:", sys.argv[1], file=sys.stderr)
-print("label probabilities of the unlabeled examples:", sys.argv[2], file=sys.stderr)
-print("features of the unlabeled examples", sys.argv[3], file=sys.stderr)
-print("output:", sys.argv[4], file=sys.stderr)
+print("unlabeled examples:", unlab_filename, file=sys.stderr)
+print("label probabilities of the unlabeled examples:", unlabprob_filename, file=sys.stderr)
+print("features of the unlabeled examples", unlabfeats_filename, file=sys.stderr)
+
+print("labeled examples:", unlab_filename, file=sys.stderr)
+print("label probabilities of the labeled examples:", unlabprob_filename, file=sys.stderr)
+print("features of the labeled examples", unlabfeats_filename, file=sys.stderr)
+
+print("output:", outfilename, file=sys.stderr)
 print("n:", n, file=sys.stderr)
 print("entr_thresh:", entr_thresh, file=sys.stderr)
 print("sim_thresh:", sim_thresh, file=sys.stderr)
 
+print("|U| =", len(mat))
+print("|L| =", len(Lmat))
+
+print("#U sentences =", len(labels))
+print("#L sentences =", len(Llabels))
+
+print("U ntokens= ", sum([len(x) for x in labels]))
+print("L ntokens= ", sum([len(x) for x in Llabels]))
+
+mat += Lmat
+labels += Llabels
+
+print("|L| + |U| =", len(mat))
+print("#U+L sentences =", len(labels))
+
+print("ntokens= ", sum([len(x) for x in labels]))
+
+
 
 #stopwords = load_set("data/stopwords.txt")
-mat_filtered, indexes_sent, indexes, entropies = pre_select(mat, labels, n=n)
+mat_filtered, indexes_sent, indexes, entropies = pre_select(mat, labels, last_unlab_index, n=n)
 print("len(mat)=", len(mat))
 
 mat_text = to_text(mat_filtered)
@@ -86,8 +129,6 @@ mat_filtered = vectorizer.fit_transform(mat_text)
 
 nselect = int(frac * len(sents))
 
-
-
 print("Computing similarities...")
 sim = cosine_distances(mat_filtered)
 
@@ -99,39 +140,48 @@ clusters = clusterer.fit_predict(sim)
 groups = {}
 selected = []
 
+num_labeled_in_cluster = {}
+
 for i, c in enumerate(clusters):
     if c not in groups:
+        num_labeled_in_cluster[c] = 0
         groups[c] = []
-    groups[c].append( (entropies[i], i) )
+    if indexes[i] > last_unlab_index:
+        num_labeled_in_cluster[c] += 1
+    groups[c].append( (entropies[i], i, indexes[i] <= last_unlab_index) )
 
 sorted_groups = []
 
-for group in groups.values():
+for group_id, group in groups.items():
     print("Cluster size:", len(group), end=" ")
     s = sorted(group)
-    sorted_groups.append(s)
+    max_entropy = s[len(s) - 1][0]
+
+    #Adds (max_entropy, group_id, and sorted elements of the group) in the list
+    sorted_groups.append( (max_entropy, group_id, s) )
+    print(max_entropy, num_labeled_in_cluster[group_id])
     for j in range(len(s)):
-        entr,i = s[len(s) - j - 1]
-        if j == 0:
-            print(entr)
-        print(mat[indexes[i]], entr)
+        entr,i,is_unlab = s[len(s) - j - 1]
+        print(mat[indexes[i]], entr, is_unlab)
     print("============\n")
 
 nrelevant_clusters = 0
+nrel_and_no_labeled = 0
 
-for group in sorted_groups:
+for max_entr, group_id, group in sorted_groups:
     if len(group) > 0:
-        entr, i = group[len(group) - 1]
-        if entr > entr_thresh:
+        if max_entr > entr_thresh:
             nrelevant_clusters += 1
-
+            if num_labeled_in_cluster[group_id] == 0:
+                nrel_and_no_labeled += 1
 
 while len(selected) < nselect:
     met_thresh = False
-    for group in sorted_groups:
+    for max_entr, group_id, group in sorted(sorted_groups, reverse=True):
         if len(group) > 0:
-            entr, i = group.pop()
-            if entr > entr_thresh:
+            is_new = num_labeled_in_cluster[group_id] == 0
+            entr, i, is_unlab = group.pop()
+            if entr > entr_thresh and is_new:
                 selected.append(indexes_sent[i])
                 met_thresh = True
                 if  len(selected) >= nselect:
@@ -139,8 +189,8 @@ while len(selected) < nselect:
     if not met_thresh:
         break
 
-print("entr_thresh, sim_thresh, #clusters, #relevant_clusters, #to_select, #selected, #coverage_of_rel_clusters:")
-print(entr_thresh, sim_thresh, len(sorted_groups), nrelevant_clusters, nselect, len(selected), len(selected)/nrelevant_clusters)
+print("entr_thresh, sim_thresh, #clusters, #relevant_clusters, #nrel_and_no_labeled, #to_select, #selected, #coverage_of_rel_clusters, #coverage_of_rel_nolabeled_clusters:")
+print(entr_thresh, sim_thresh, len(sorted_groups), nrelevant_clusters, nrel_and_no_labeled, nselect, len(selected), len(selected)/nrelevant_clusters, len(selected)/nrel_and_no_labeled)
 
 #Print selected sentences
 for i in selected:
