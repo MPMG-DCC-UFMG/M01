34af389e9fba812b836136384e3775a1239023a5
Fabiano <famube@gmail.com>
2022-06-28 15:50:05 -0300

    Conversor de formatos

--- preprocessing/json_formater.py
+++ preprocessing/json_formater.py
@@ -1,6 +1,8 @@
 import json
 
+PUNCT=".,;?!:"
 
+#Funcoes de convercao de formatos
 
 def to_iob(jdata, outfile):
     for obj in jdata:
@@ -29,6 +31,50 @@ def to_iob(jdata, outfile):
         print(file=outfile)
 
 
+def to_char_level_format(jdata):
+    segments = []
+    for dic in jdata:
+        idx_token2char = {}
+        tokens = dic["tokens"]
+        char_idx = 0
+        tokens_and_spaces = []
+        if len(tokens) > 0:
+            tokens_and_spaces.append(tokens[0])
+            idx_token2char[0] = char_idx
+            char_idx += len(tokens[0])
+        for i in range(1, len(tokens)):
+            token = tokens[i]
+            if token in PUNCT:
+                tokens_and_spaces.append(token)
+            else:
+                tokens_and_spaces.append(" ")
+                tokens_and_spaces.append(token)
+                char_idx += 1
+            idx_token2char[i] = char_idx
+            char_idx += len(token)
+        segment_text = "".join(tokens_and_spaces)
+        new_entities = []
+        if "entities" in dic:
+            entities = dic["entities"]
+            for ent in entities:
+                start = ent["start"]
+                end = ent["end"] - 1
+                start_char = idx_token2char[start]
+                end_char = idx_token2char[end] + len(tokens[end])
+                new_entities.append({"entity": segment_text[start_char:end_char],
+                                     "start": start_char, "end": end_char,
+                                     "label": ent["type"]})
+        new_relations = []
+        if "relations" in dic:
+            relations = dic["relations"]
+            for rel in relations:
+                new_relations.append({"entities": [rel["head"], rel["tail"]],
+                                      "label": rel["type"]})
+        segments.append({"text": segment_text, 
+                         "entities": new_entities, 
+                         "relations": new_relations})
+        return segments
+
 def match_tokens(ent_str, tokens, start_from=0):
     token_idx = start_from
     char2token = {}
@@ -52,6 +98,9 @@ def match_tokens(ent_str, tokens, start_from=0):
         return char2token[c], 1+char2token[c+len(to_match)-1]
     else:
         return -1, 0
+
+
+
 
 class JsonFormater:
     #tokenizer: spaCy model
@@ -84,15 +133,19 @@ class JsonFormater:
         entities = []
         not_found = 0
         end = 0
+        ent_map = {}
         for start_char, end_char, label in sorted(ents):
             ent_str = text[start_char:end_char]
             start,end = match_tokens(ent_str, tokens, start_from=end)
             if start != -1:
-                entities.append({"start":start, "end":end, "type":label})
+                entities.append({"start":start, "end":end, "type":label, "score":1})
             else:
                 not_found += 1
+        orig_ents = []
+        if "entities" in jdoc:
+            orig_ents = jdoc["entities"] 
         #print("Not matched entities:", not_found)
-        return {"tokens":tokens, "entities": entities}
+        return {"tokens":tokens, "entities": entities + orig_ents}
 
 
     #TO UPDATE
@@ -131,10 +184,11 @@ if __name__ == '__main__':
     tokenizer = spacy.load("pt_core_news_sm")
 
     formater = JsonFormater(tokenizer)
-    data = formater.format(u"O Nobre Rato roeu a roupa do rei de Roma.", [(2, 12, "PER"), (36, 40, "LOC")])
+    data = formater.format(u"O Nobre Rato roeu a roupa do rei de Roma.", [(2, 12, "PER"), (38, 42, "LOC")])
                             #012345678901234567890123456789012345678901
                             #0         1         2         3         4
-    print(json.dumps(data, indent=4))
-
+    print(json.dumps([data], indent=4))
+    segs = to_char_level_format([data])
+    print(json.dumps(segs, indent=4))
 
 
