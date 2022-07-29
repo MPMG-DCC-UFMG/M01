6d2def0e8e8937c0f62c0cd8e66899d015962f0f
famube <famube@gmail.com>
2022-07-29 15:56:11 -0300

    Extrator de relações basedo em regras

---
+++ relations/main.py
@@ -0,0 +1,69 @@
+import sys
+import json
+#from nltk.metrics.distance import edit_distance
+from preprocessing.casing import title_case
+from preprocessing.text_cleaner import extract_digits, tokenize
+from inout import read_lower_cased_strings
+from prefix_tree import *
+from entity import Entity
+from relation_extractor import RelationExtractor
+import re
+
+DATE_PATTERN = re.compile("(\d\d?\s?/\s?\d\d?/\s?\d\s?\.?\s?\d(\d\d)?)|(\d\d?\s?\-\s?\d\d?\-\s?\d\s?\.?\s?\d(\d\d)?)|(\d\d?\s?\.\s?\d\d?\.\s?\d\s?\.?\s?\d(\d\d)?)")
+MUNICIPIOS = read_lower_cased_strings("data/municipios.txt")
+WSIZE = 100
+MODALIDADES = ["convite", "tomada de preços", "concorrência", "concurso", "pregão presencial", "pregão eletrônico", "leilão"]
+TIPOS = ["melhor técnica", "menor preço", "maior lance ou oferta", "técnica e preço"]
+INF = 999999999
+
+TREE_MUNICIPIOS = make_dictionary(MUNICIPIOS)
+
+#TREE_MODALIDADES.print_tree()
+#TREE_TIPOS.print_tree()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print ("usage: %s <input file (.json produced by NER)> <outfile (.json with relations included)>" % sys.argv[0])
+        sys.exit(-1)
+
+    filename = sys.argv[1]
+    outname = sys.argv[2]
+    infile = open(filename, encoding="utf-8")
+    outfile = open(outname, "w", encoding="utf-8")
+    data = json.load(infile)
+    infile.close()
+
+    if "sentences" in data:
+        segments = data["sentences"]
+    else:
+        segments = [data]
+
+    for segment in segments:
+
+        # entities: vetor de entidades na ordem em que elas aparecem no texto
+
+        entities = sorted([Entity(ent["start"], ent["end"], ent["entity"], ent["label"]) for ent in segment["entities"]])
+        for i, ent in enumerate(entities):
+            entities[i].idx = i
+        text = segment["text"]
+        relation_extractor = RelationExtractor(entities, text)
+        rels = [rel.to_dict() for rel in relation_extractor.extract_relations()]
+        ents = [ent.to_dict() for ent in entities]
+        segment["relations"] = rels
+        segment["entities"] = ents
+
+        for rel in rels:
+            print(rel)
+
+    #norm_text = remover_acentos(orig_text).lower()
+    #tokenized = tokenize(norm_text)
+    #print(tokenized)
+
+
+
+    json.dump(data, outfile, indent=4)
+    outfile.close()
+
+
+
