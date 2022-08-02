1e60bd5879f3d16df57e223fdcf6016613178f92
famube <famube@gmail.com>
2022-08-02 13:00:45 -0300

    Script de filtragem de entidades

---
+++ nerre/scripts/data_cleaning/filter_entities.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+
+def filter(jdata, keep="PESSOA ORGANIZACAO LEGISLACAO".split()):
+    res = []
+    for dic in jdata:
+        tokens = dic["tokens"]
+        entities = dic["entities"]
+        new_ents = []
+        for ent in entities:
+            if ent["type"] in keep:
+                new_ents.append(ent)
+        res.append({"tokens": tokens, "entities": new_ents, "relations": []})
+    return res
+
+infile = open(sys.argv[1], encoding="utf-8")
+outfile = open(sys.argv[2], "w", encoding="utf-8")
+jdata = json.load(infile)
+infile.close()
+
+new_jdata = filter(jdata)
+
+json.dump(new_jdata, outfile, indent=4)
+outfile.close()
+
