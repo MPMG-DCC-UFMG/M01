124c637545362bab1910384c3b6e0fb7bd5f702b
famube <famube@gmail.com>
2022-09-26 20:46:44 -0300

    script - filtragem de dados

---
+++ nerre/scripts/converters/filter_json.py
@@ -0,0 +1,18 @@
+import sys
+import json
+
+infile = open(sys.argv[1], encoding="utf-8")
+outfile = open(sys.argv[2], "w", encoding="utf-8")
+data = json.load(infile)
+res = []
+
+for item in data:
+    tokens = item["tokens"]
+#    print(len(tokens))
+    if len(tokens) > 150:
+        continue
+    res.append( item )
+
+print("%%filtered:", len(res) / len(data))
+json.dump(res, outfile, indent=4)
+
