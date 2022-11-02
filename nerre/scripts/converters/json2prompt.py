2c8f07347fdc2ab8e3df1063d31e74c90a8fbd2f
famube <famube@gmail.com>
2022-11-02 16:37:50 -0300

    scripts prompts gpt

---
+++ nerre/scripts/converters/json2prompt.py
@@ -0,0 +1,52 @@
+import sys
+import json
+
+PUNCT = ",;.?!:"
+
+
+#Junta novamente palavras que foram separadas pelo tokenizer
+def tokens2sentence(tokens):
+    tokens_with_spaces = []
+    for token in tokens:
+        if token not in PUNCT:
+            tokens_with_spaces.append(" " + token)
+        else:
+            tokens_with_spaces.append(token)
+    return "".join(tokens_with_spaces)[1:]
+
+
+def convert(data):
+    res = []
+    for dic in data:
+        rows = []
+        text = tokens2sentence(dic["tokens"])
+        rows.append(f"[Texto]: {text}")
+        ent_strs = []
+        added_ents = set()
+        for ent in dic["entities"]:
+            label = ent["type"].lower()
+            ent_str = " ".join(tokens[ent["start"]:ent["end"]])
+            ent_strs.append(ent_str)
+            ent_str_low = ent_str.lower()
+            if ent_str_low not in added_ents:
+                rows.append(f"[entidade]: {label}: {ent_str}")
+                added_ents.add(ent_str_low)
+        for rel in dic["relations"]:
+            label = rel["type"].lower()
+            head = ent_strs[rel["head"]]
+            tail = ent_strs[rel["tail"]]
+            rows.append(f"[relação]: {head}; {label}; {tail}")
+        res.append("\n".join(rows))
+    return "###".join(res)
+
+
+
+infile = open(sys.argv[1], encoding="utf-8")
+outfile = open(sys.argv[2], "w", encoding="utf-8")
+data = json.load(infile)
+infile.close()
+s = convert(data)
+print(s, file=outfile)
+outfile.close()
+
+
---
+++ nerre/scripts/sampler.py
@@ -0,0 +1,47 @@
+import json
+import sys
+import random
+
+random.seed(42)
+
+
+def complementarity_score(data, candidates, covered_rel_types):
+    scores = []
+    for i in candidates:
+        dic = data[i]
+        if len(dic["tokens"]) > 120:
+            scores.append((0, i))
+            continue
+        relations = dic["relations"]
+        item_types = set([rel["type"] for rel in relations])
+        diff = item_types - covered_rel_types
+        scores.append((len(diff), i))
+
+def rank(data):
+    selected = []
+    candidates = set(list(range(len(data))))
+    covered_rel_types = set()
+    for i in range(len(data)):
+        scores = complementarity_score(data, candidates, covered_rel_types)
+        s = max(scores)[1]
+        selected.append(s)
+        candidates.remove(s)
+        covered_rel_types.update([rel["type"] for rel in data[s]["relations"]])
+    return selected
+
+
+
+infile = open(sys.argv[1], encoding="utf-8")
+outfile = open(sys.argv[2], "w", encoding="utf-8")
+
+data = json.load(infile)
+infile.close()
+
+random.shuffle(data)
+
+selected = rank(data)
+
+res = [data[s] for s in selected]
+
+json.dump(res, indent=4)
+
